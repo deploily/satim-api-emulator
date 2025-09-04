2fce71c913c96f7a390e30816cbc59baf4ac61ab
azziimalek <malek.azzi@transformatek.dz>
2025-09-04 13:00:34 +0000

    Fix api file.

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,13 +4,12 @@
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->prefix("payment/rest")->group(function () {
+Route::prefix("payment/rest")->group(function () {
     Route::get("register.do", [PaymentController::class, 'register']);
     Route::get("confirmOrder.do", [PaymentController::class, 'confirm']);
     Route::get("refund.do", [PaymentController::class, 'refund']);
     Route::get("generateCredentials.do", [PaymentController::class, 'generateCredentials']);
 });
-
 
  
 
